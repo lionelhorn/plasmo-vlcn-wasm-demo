4f135355e6a8fa4b132c6d228175e2117a82fd13
Lionel <dev@lionelhorn.com>
2024-02-01 18:57:21 +0100

    docs: add readme

---
+++ README.md
@@ -0,0 +1,22 @@
+### Simple starter with
+
+* Plasmo (browser extension framework)
+* VLCN crsqlite (sqlite db in browser)
+
+### Dev
+Install deps and build
+```shell
+pnpm install
+pnpm dev
+```
+Install extension in chrome
+
+* Navigate to `chrome://extensions/`
+* Add unpackaged app from `build/chrome-mv3-dev` in browser. (Activate dev mode first)
+* Click on `service worker`
+
+In the console you'll see data saved in sqlite and retrieved back.
+
+It should look like
+![img_1.png](img_1.png) 
+
--- src/background/index.ts
+++ src/background/index.ts
@@ -4,12 +4,12 @@ import wasmUrl from "raw:@vlcn.io/crsqlite-wasm/crsqlite.wasm";
 (async () => {
 	const sqlite = await initWasm(() => wasmUrl);
 
-	const db = await sqlite.open("my-database.db");
+	const db = await sqlite.open("database.db");
 	await db.exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY NOT NULL, name TEXT)");
 
 	const stmt = await db.prepare("INSERT OR IGNORE INTO users (name) VALUES (?)");
 	await stmt.run(db, `John Do. Born at ${new Date().toTimeString()}`);
 
 	const users = await db.execA<[bigint, string]>("SELECT * FROM users");
-	console.log(users);
+	console.log("Saved users", users);
 })()
