fddedfc5bfd2b7f0d1602b7694976dbef02f662d
Lionel <dev@lionelhorn.com>
2024-02-01 18:43:29 +0100

    feat: first commit

---
+++ src/background/index.ts
@@ -0,0 +1,15 @@
+import initWasm from "@vlcn.io/crsqlite-wasm";
+import wasmUrl from "raw:@vlcn.io/crsqlite-wasm/crsqlite.wasm";
+
+(async () => {
+	const sqlite = await initWasm(() => wasmUrl);
+
+	const db = await sqlite.open("my-database.db");
+	await db.exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY NOT NULL, name TEXT)");
+
+	const stmt = await db.prepare("INSERT OR IGNORE INTO users (name) VALUES (?)");
+	await stmt.run(db, `John Do. Born at ${new Date().toTimeString()}`);
+
+	const users = await db.execA<[bigint, string]>("SELECT * FROM users");
+	console.log(users);
+})()
